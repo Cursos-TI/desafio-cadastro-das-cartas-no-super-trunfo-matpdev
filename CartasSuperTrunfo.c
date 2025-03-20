d197485844a98e5e4b963460a2b8917c008b6fee
Matheus Nunes <matheus2ep@gmail.com>
2025-03-19 23:18:48 -0300

    feat(menus): added new menus

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
 struct Card
 {
@@ -16,8 +19,8 @@ int menu()
 start:
   int selected_item;
 
-  printf("==========================\n");
-  printf("MENU DO SUPER TRUNFO\n");
+  printf("\n\n==========================\n");
+  printf("       MENU DO SUPER TRUNFO\n");
   printf("==========================\n");
 
   printf("\n");
@@ -45,53 +48,197 @@ start:
   }
 }
 
-int main()
+int create_cards(struct Card *cards[5], int start_at)
 {
-  struct Card cards[5] = {};
-  int finishedCount = 0;
+  int index_start = start_at;
+  char need_continue;
 
-  int selected = menu();
-
-  printf("ITEM SELECTED = %d", selected);
-
-  if (selected == -1)
+  if (index_start <= 0)
   {
-    return 1;
+    index_start = 0;
+  }
+  else if (index_start > 0)
+  {
+    index_start--;
   }
 
-  // printf("Olá, vamos iniciar o super trunfo de países?\n");
-  // if (finishedCount < 2)
-  // {
-  //   printf("Antes de iniciarmos, precisamos criar nossas cartas\n");
-  //   printf("Precisamos de no mínimo 2 cartas");
-  // }
+  printf("Vamos criar as carta.\n");
+  for (int i = index_start; i < 5; i++)
+  {
+    if (i == 2 && need_continue != 'S')
+    {
+      printf("Você criou o mínimo de 2 cartas, deseja continuar criando?\n");
+      printf("S para sim e N para não\n");
+      scanf(" %c", &need_continue);
 
-  //  float area, pib;
-  //    int touristSpot, population;
+      if (need_continue == 'N')
+      {
+        return index_start;
+      }
+    }
 
-  //
-  //    printf("Vamos criar a primeira.\n");
-  //
-  //    printf("Diga me a área do país, modelo: 10.0\n");
-  //    scanf("%f", &area);
-  //
-  //    printf("Agora preciso da população, modelo: 1000\n");
-  //    scanf("%d", &population);
-  //
-  //    printf("Agora, preciso do PIB deste país\n");
-  //    scanf("%f", &pib);
-  //
-  //    printf("E por ultimo, preciso da quantidade de pontos turísticos\n");
-  //    scanf("%d", &touristSpot);
-  //
-  //    printf("\n\n============== PRIMEIRA CARTA CADASTRADA
-  //    ============\n\n\n");
-  //
-  //    printf("Quantidade de pontos turísticos: %d\n", touristSpot);
-  //    printf("População total de %d\n\n", population);
-  //
-  //    printf("Área total do país será de %.2f\n", area);
-  //    printf("PIB desta país é será de %.2f\n", pib);
+    printf("%d° carta \n", i + 1);
+
+    printf("Digite a área do país, modelo: 10.0 -> ");
+    if (scanf("%f", &cards[i]->area) != 1)
+    {
+      printf("Erro ao ler a área do país %d!\n", i + 1);
+      return 1; // Return error code if scanf fails
+    }
+    printf("Área do país: %.2f\n", cards[i]->area);
+
+    printf("Digite a população do país, ex: 1000 -> ");
+    if (scanf("%d", &cards[i]->population) != 1)
+    {
+      printf("Erro ao ler a população do país %d!\n", i + 1);
+      return 1; // Return error code if scanf fails
+    }
+    printf("População do país: %d\n", cards[i]->population);
+
+    printf("Digite o PIB do país, ex: 1000.10 -> ");
+    if (scanf("%f", &cards[i]->pib) != 1)
+    {
+      printf("Erro ao ler a PIB do país %d!\n", i + 1);
+      return 1; // Return error code if scanf fails
+    }
+    printf("PIB do país: %.2f\n", cards[i]->pib);
+
+    printf("Por ultimo, a quantidade de pontos turisticos do país, ex: 1000 -> ");
+    if (scanf("%d", &cards[i]->tourist_spots) != 1)
+    {
+      printf("Erro ao ler a PIB do país %d!\n", i + 1);
+      return 1; // Return error code if scanf fails
+    }
+    printf("Quantidade de pontos turisticos do país: %d\n", cards[i]->tourist_spots);
+
+    index_start = i + 1;
+
+    if (cards[i]->area != 0)
+    {
+      cards[i]->population_density = cards[i]->area / cards[i]->population;
+    }
+    else
+    {
+      cards[i]->population_density = 0;
+    }
+
+    if (cards[i]->pib != 0)
+    {
+      cards[i]->pib_head = cards[i]->pib / cards[i]->population;
+    }
+    else
+    {
+      cards[i]->pib_head = 0;
+    }
+  }
+
+  printf("INDEX START %d", index_start);
+
+  return index_start + 1;
+}
+
+void clear_terminal()
+{
+#ifdef _WIN32
+  system("cls"); // For Windows
+#else
+  system("clear"); // For Linux/macOS
+#endif
+}
+
+void delay(int milliseconds)
+{
+  clock_t start_time = clock();
+  while (clock() < start_time + milliseconds * CLOCKS_PER_SEC / 1000)
+    ;
+}
+
+int compare_cards(struct Card *cards[5], int total_cards_added)
+{
+  int first_card;
+  int second_card;
+
+  clear_terminal();
+
+  printf("\n\nAgora vamos iniciar nosso Super Truco\n");
+  printf("---------------------------------------\n\n");
+  printf("Você criou algumas cartas, qual delas quer comparar primeiro?\n\n");
+  printf("Irei lhe mostrar os dados de todas que criou\n\n");
+  delay(1000);
+
+  for (int i = 0; i < total_cards_added; i++)
+  {
+    // cards[i]
+    printf("\nCarta Número %d\n----------------\n", i + 1);
+    printf("Área do país: %.2f\n", cards[i]->area);
+    printf("População do país: %d\n", cards[i]->population);
+    printf("PIB do país: %.2f\n", cards[i]->pib);
+    printf("Quantidade de pontos turisticos do país: %d\n", cards[i]->tourist_spots);
+
+    printf("PIB per capita da carta: %.2f\n", cards[i]->pib_head);
+    printf("Densidade Populacional da carta: %.2f\n\n", cards[i]->population_density);
+    delay(2000);
+  }
+
+  for (int i = 0; i < total_cards_added; i++)
+  {
+    printf("Carta [%d]", i + 1);
+    if (i < total_cards_added - 1)
+    {
+      printf(" | ");
+    }
+  }
+
+  printf("Escolha qual das cartas deseja ser sua carta: ");
+  scanf("%d\n", &first_card);
+  printf("Escolha qual das cartas deseja será do seu adversário: ");
+  scanf("%d\n", &second_card);
+
+  printf("\n");
+}
+
+int main()
+{
+  struct Card *cards[5] = {NULL};
+  int finishedCount = 0;
+
+  for (int i = 0; i < 5; i++)
+  {
+    cards[i] = (struct Card *)malloc(sizeof(struct Card));
+    if (cards[i] == NULL)
+    {
+      printf("Ocorreu um erro ao alocar dados na memoria %d\n", i);
+      return 1;
+    }
+  }
+
+  while (1)
+  {
+    // back_to_menu:
+    int selected = menu();
+    if (selected == -1)
+    {
+      return 0; // Exit the program
+    }
+
+    if (finishedCount < 2)
+    {
+      printf("Antes de iniciarmos, precisamos criar nossas cartas\n");
+      printf("Precisamos de no mínimo 2 cartas\n");
+
+      finishedCount = create_cards(cards, finishedCount);
+    }
+
+    if (finishedCount >= 2)
+    {
+      compare_cards(cards, finishedCount);
+    }
+  }
+
+  for (int i = 0; i < 5; i++)
+  {
+    free(cards[i]);
+  }
 
   return 0;
 }
